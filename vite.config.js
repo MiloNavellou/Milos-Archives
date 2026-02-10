37d8181c321b6ef598a6cf87cae522bc529dbfb1
Milo Navellou <milo.navellou@gmail.com>
2026-02-10 14:13:30 +0100

    Optimisation Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+
+
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-    base: '/Milos-Archives/',
-  server: {
+export default defineConfig(({ command, mode }) => {
+  return {
+    // Si on build pour GitHub Pages, on utilise le nom du repo
+    // Sinon (Vercel, Local), on utilise la racine '/'
+    base: process.env.GITHUB_ACTIONS ? '/Milos-Archives/' : '/',
+    server: {
     host: true // Cela expose le serveur sur ton réseau local
+  }
   }
 })
